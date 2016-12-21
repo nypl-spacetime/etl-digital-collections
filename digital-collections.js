da75695ccdf64efcf69b5607e0941f4c8b0b58b3
Bert Spaan <bert.spaan@gmail.com>
2016-12-21 17:45:20 -0500

    support multiple datasets

--- digital-collections.js
+++ digital-collections.js
@@ -4,35 +4,85 @@ const R = require('ramda')
 const H = require('highland')
 const mkdirp = require('mkdirp')
 
-const oldnycFile = '/Users/bertspaan/data/spacetime/etl/transform/oldnyc/oldnyc.objects.ndjson'
+const getFilename = (currentDir, dataset) =>
+  path.join(currentDir, '..', '..', 'transform', dataset, `${dataset}.objects.ndjson`)
 
-function createFile (dir, data) {
-  const filename = path.join(dir, ...data.uuid.split('-')) + '.json'
-  const contents = {
-    oldnyc: {
-      locations: data.locations.map((location) => ({
-        id: location.id,
-        url: location.data.url,
-        geometry: location.geometry
-      }))
-    }
+const datasets = {
+  'oldnyc': {
+    getUuid: (object) => object.data && object.data.uuid,
+    transformToDc: (object) => ({
+      id: object.id,
+      url: object.data.url,
+      geometry: object.geometry
+    })
+  },
+  'mapwarper': {
+    getUuid: (object) => object.data && object.data.uuid,
+    transformToDc: (object) => ({
+      id: object.id,
+      url: `http://maps.nypl.org/warper/maps/${object.id}`,
+      geometry: object.geometry
+    })
   }
-
-  mkdirp.sync(path.dirname(filename))
-  fs.writeFileSync(filename, JSON.stringify(contents, null, 2))
 }
 
-function aggregate (config, dirs, tools, callback) {
-  H(fs.createReadStream(oldnycFile))
+function getLines (obj) {
+  return H(fs.createReadStream(obj.filename))
     .split()
     .compact()
     .map(JSON.parse)
-    .filter((object) => object.data.uuid)
-    .group((object) => object.data.uuid)
+    .map((object) => ({
+      dataset: obj.dataset,
+      object
+    }))
+}
+
+function writeContents(dir, uuid, contents, callback) {
+  // const filename = path.join(dir, ...uuid.split('-')) + '.json'
+  // mkdirp.sync(path.dirname(filename))
+
+  // const filename = path.join(dir, `${uuid}.json`)
+  // fs.writeFileSync(filename, JSON.stringify(contents, null, 2))
+
+  callback()
+}
+
+function writeLines (dir, uuid, lines, callback) {
+  H(lines)
+    .group('dataset')
     .map(R.toPairs)
     .sequence()
-    .map(R.zipObj(['uuid', 'locations']))
-    .each(H.curry(createFile, dirs.current))
+    .map(R.zipObj(['dataset', 'lines']))
+    .map((data) => Object.assign(data, {
+      lines: data.lines.map((line) => datasets[line.dataset].transformToDc(line.object))
+    }))
+    .errors(console.error)
+    .map((data) => ([
+      data.dataset, data.lines
+    ]))
+    .toArray((data) => {
+      const contents = R.fromPairs(data)
+      writeContents(dir, uuid, contents, callback)
+    })
+}
+
+function aggregate (config, dirs, tools, callback) {
+  H(Object.keys(datasets))
+    .map((dataset) => ({
+      dataset,
+      filename: getFilename(dirs.current, dataset)
+    }))
+    .map(getLines)
+    .sequence()
+    .filter((line) => datasets[line.dataset].getUuid && datasets[line.dataset].transformToDc)
+    .filter((line) => datasets[line.dataset].getUuid(line.object))
+    .group((line) => datasets[line.dataset].getUuid(line.object))
+    .map(R.toPairs)
+    .sequence()
+    .map(R.zipObj(['uuid', 'lines']))
+    .map((uuidLines) => H.curry(writeLines, dirs.current)(uuidLines.uuid, uuidLines.lines))
+    .nfcall([])
+    .series()
     .done(callback)
 }
 
